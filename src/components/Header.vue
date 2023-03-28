7eb1f168f1d518b4d59fa4744d01ce63153df712
donghyun960215 <tlzma789@naver.com>
2023-03-29 08:46:07 +0900

    23.03.29 출근전 공부

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,9 +7,11 @@
         class="nav-item">
         <RouterLink
           :to="nav.href"
-          active-class="active"
+          active-class="active"  
           class="nav-link">
           {{ nav.name }}
+          <!-- active-class="active" 은 원래는 router 의 기본 속성인 클래스 이름이들어가 있다. router-link-active 이렇게
+          들어가 있으면 부트스트랩의 속성이 적용이 안되므로 active 클래스 명을 변경을 해준다. -->
         </RouterLink>
       </div>
     </div>
