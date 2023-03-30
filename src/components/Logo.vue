54c880e98ab5dddfb0793778bcedbe2f5cd8368d
donghyun960215 <tlzma789@naver.com>
2023-03-30 22:31:23 +0900

    23.03.30 퇴근 후 공부

---
+++ src/components/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <RouterLink
+    to="/"
+    class="logo">
+    <span>OMDbAPI</span>.COM
+  </RouterLink>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+.logo {
+  font-family: "Oswald", sans-serif;
+  font-size: 20px;
+  color: $black; //bootstrap 사용
+  text-decoration: none;
+  &:hover {
+    color: $black;
+  }
+  span {
+    color: $primary;
+  }
+}
+</style>
