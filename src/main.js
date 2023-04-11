78dc38c0b10f0da3705779004fe073234aa9c91b
donghyun960215 <tlzma789@naver.com>
2023-04-11 12:52:53 +0900

    23.04.11 점심시간 공부

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'  //특정한 폴더의 index 파일을 가져올 때에는 파일명 생략이 가능하다.
 import store from './store'
+import loadImage from './plugins/loadImage'
 
 
 createApp(App)
-  .use(router)
-  .use(store)
+  .use(router)      //$route, $router
+  .use(store)       //$store
+  .use(loadImage)   //$loadImage
   .mount('#app')
