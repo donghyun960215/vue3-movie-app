07ff683465b4fc32c37b070d2b11c1b6120bec4d
donghyun960215 <tlzma789@naver.com>
2023-04-18 10:27:48 +0900

    23.04.18 공부

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import MovieItem from '~/components/Movieitem'
+import MovieItem from '~/components/MovieItem'
 import Loader from '~/components/Loader'
 import { mapState} from 'vuex'
 
