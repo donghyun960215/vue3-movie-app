3d0745a8b6147dd24ea2af7e8e9a59638a4885c7
donghyun960215 <tlzma789@naver.com>
2023-04-20 19:33:58 +0900

    23.04.20 project 완료

--- functions/movie.js
+++ functions/movie.js
@@ -1,10 +1,12 @@
 const axios = require('axios')
+//const OMDB_API_KEY = process.env.OMDB_API_KEY .env를 사용한다 맡애처 객체구조분해를 사용하여 간략하게 만든다
+const {OMDB_API_KEY} = process.env
 
 exports.handler = async function (event) {
   console.log(event)
   const payload = JSON.parse(event.body) //문자데이터로 넘어오는 데이터를 JSON.parse를 사용하여 다시 객체데이터로 변환
   const {title, type, year, page, id} = payload
-  const OMDB_API_KEY = '7035c60c'
+  //const OMDB_API_KEY = '7035c60c' 원래는 옆에와 같이 사용했지만 보안 문제로인해 환경변수를 사용한다. .env를 사용한다.
   const url = id
     ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
     : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const path = _require('path')
 const HtmlPlugin = _require('html-webpack-plugin')
 const CopyPlugin = _require('copy-webpack-plugin')
 const { VueLoaderPlugin } = _require('vue-loader')
+const Dotenv = _require('dotenv-webpack')
 
 module.exports = {
   resolve: {
@@ -79,7 +80,7 @@ module.exports = {
       ]
     }),
     new VueLoaderPlugin(),
-    
+    new Dotenv()
   ],
 
   // 개발 서버 옵션
